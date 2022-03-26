97abb4c5d770c86d49576cc14b23d3bc38edbb7d
Vicktoria853 <48590084+Vicktoria853@users.noreply.github.com>
2022-03-26 21:43:52 +0200

    Adding psychrometer
    Very long equations taken by 
    Part 1: https://www.youtube.com/watch?v=ZLkBXbj8pAM
    Part 2: https://www.youtube.com/watch?v=euEMgufpKX4

---
+++ Microcontroller/Psychrometer.cpp
@@ -0,0 +1,47 @@
+#ifndef PSYCHROMETER_CPP
+#define PSYCHROMETER_Cpp
+
+#include "Psychrometer.h"
+
+float PsychrometerCal::calculatePsat(float temp){
+  helper = 0;
+  helper = c8/temp + c9 + c10*temp + c11*(pow(temp, 2)) + c12*(pow(temp, 3)) + c13*(log(temp));
+  return pow(helper, helper);
+  }
+  
+//float PsychrometerCal::calculateOmegaSat(float Db, float Wb){
+//  helper = 0;
+//  helper = (2501 - (PsatTwb*Wb / 1000))*wSatTwb - 1.006*(Db - Wb); // why /1000 in video?
+//  w = w/ (2501 + (1.805*Db) - (4.186*Wb));
+//  
+//  }
+
+
+float PsychrometerCal::calculating(  float dryTempAdded, float wetTempAdded){
+  dryTemp = dryTempAdded;
+  dryTempK = dryTemp + K;
+  wetTemp = wetTempAdded;
+  wetTempK = wetTemp + K;
+  
+  PsatTwb = calculatePsat(wetTempK);
+  wSatTwb = 0;
+  wSatTwb = (0.622*PsatTwb)/(Pt - PsatTwb);
+
+  w = (2501 - (PsatTwb*wetTemp / 1000))*wSatTwb - 1.006*(dryTemp - wetTemp); // why /1000 in video?
+  w = w/ (2501 + (1.805*dryTemp) - (4.186*wetTemp));
+  
+  PsatTdb = calculatePsat(dryTempK);
+  wSatTdb = 0;
+  wSatTdb = (0.622*PsatTwb)/(Pt - PsatTdb);
+
+  mu = w/wSatTdb; // degree of saturation
+
+  RelativeHumidity = mu / (1 - (1 - mu)*(PsatTdb/Pt));
+  
+  
+  }
+
+
+  
+
+#endif
---
+++ Microcontroller/Psychrometer.h
@@ -0,0 +1,52 @@
+#ifndef PSYCHROMETER_H
+#define PSYCHROMETER_H
+
+#include <math.h> 
+
+class PsychrometerCal{
+  public:
+  float dryTemp;
+  float dryTempK;// in Kelvins
+  float wetTemp;
+  float wetTempK; // in Kelvins
+
+  float Pt; // 101 kPa, default 
+  
+  const float K = 273.15; 
+  const float c8 = -5800.2206;// -5.8002206 * 1000
+  const float c9 = 1.3914993;
+  const float c10 = -0.048640239;  // -4.8640239 * (1/100)
+  const float c11 = 4.1764768 * (1/10000);
+  const float c12 = -1.4452093 * pow(10, -8);
+  const float c13 = 6.5459673;
+
+  const float e = 2.71828182;
+
+  float helper;
+
+
+  float PsatTwb; // 
+  float PsatTdb;
+  float wSatTwb;
+  float wSatTdb;
+  float w;
+
+  float mu;
+
+  float RelativeHumidity; // tthe result
+
+  
+  float calculatePsat(float temp);
+  float calculating(  float dryTempAdded, float wetTempAdded);
+  
+
+  
+  
+  
+  
+  
+};
+
+
+
+#endif
